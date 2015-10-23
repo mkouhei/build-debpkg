6cbb52cd8b64a541aba88393d0709210413db15b
Kouhei Maeda <mkouhei@palmtb.net>
2015-10-23 15:47:16 +0900

    Installs debian-archive-keyring, adds keyring option.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -ev
 
 sudo apt-get update -qq
-sudo apt-get install -y pbuilder devscripts
-test ! -f $HOME/base.tgz && sudo pbuilder --create --basetgz $HOME/base.tgz --distribution sid --components main --mirror http://http.debian.net/debian
+sudo apt-get install -y pbuilder devscripts debian-archive-keyring
+test ! -f $HOME/base.tgz && sudo pbuilder --create --basetgz $HOME/base.tgz --distribution sid --components main --mirror http://http.debian.net/debian --keyring /usr/share/keyrings/debian-archive-keyring.gpg
 sudo pbuilder --update --basetgz $HOME/base.tgz
 
 dget http://http.debian.net/debian/pool/main/p/pystache/pystache_0.5.4-5.dsc
