50b85785e7dc11d11af22b5f03594d2106359a2c
Kouhei Maeda <mkouhei@palmtb.net>
2015-10-23 15:35:23 +0900

    Changes using shell script instead travis hooks.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ run-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -ev
+
+sudo apt-get update -qq
+sudo apt-get install -y pbuilder devscripts
+test ! -f $HOME/base.tgz && sudo pbuilder --create --basetgz $HOME/base.tgz --distribution sid --components main
+sudo pbuilder --update --basetgz $HOME/base.tgz
+
+dget http://http.debian.net/debian/pool/main/p/pystache/pystache_0.5.4-5.dsc
+sudo pbuilder --build pystache_0.5.4-5.dsc
