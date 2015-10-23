2175bbd8360956132a5803ed1ea7a5ce499a5369
Kouhei Maeda <mkouhei@palmtb.net>
2015-10-23 16:12:34 +0900

    Installs debian-keyring/precise-backports.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@
 sudo sh -c "echo deb http://archive.ubuntu.com/ubuntu precise-backports main restricted universe multiverse >> /etc/apt/sources.list"
 sudo apt-get update -qq
 sudo apt-get install -y pbuilder devscripts
-sudo apt-get install -y debian-archive-keyring/precise-backports
+sudo apt-get install -y debian-archive-keyring/precise-backports debian-keyring/precise-backports
 test ! -f $HOME/base.tgz && sudo pbuilder --create --basetgz $HOME/base.tgz --distribution sid --components main --mirror http://http.debian.net/debian --debootstrapopts "--keyring=/usr/share/keyrings/debian-archive-keyring.gpg"
 sudo pbuilder --update --basetgz $HOME/base.tgz
 
