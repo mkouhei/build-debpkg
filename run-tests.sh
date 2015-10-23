8093e75b8203117d917068744ba11c3752858dd0
Kouhei Maeda <mkouhei@palmtb.net>
2015-10-23 21:22:40 +0900

    Changes to use apt-get source from Sid.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- run-tests.sh
+++ run-tests.sh
@@ -2,10 +2,13 @@
 
 sudo sh -c "echo deb http://archive.ubuntu.com/ubuntu precise-backports main restricted universe multiverse >> /etc/apt/sources.list"
 sudo sh -c "echo deb http://archive.ubuntu.com/ubuntu wily universe >> /etc/apt/sources.list"
+sudo sh -c "echo deb http://http.debian.net/debian sid main >> /etc/apt/sources.list"
+sudo sh -c "echo deb-src http://http.debian.net/debian sid main >> /etc/apt/sources.list"
 sudo apt-get update -qq
 sudo apt-get install -y pbuilder devscripts
 sudo apt-get install -y debian-archive-keyring/precise-backports
 sudo apt-get install -y debian-keyring/wily
 sudo pbuilder --create --basetgz $HOME/base.tgz --distribution sid --components main --mirror http://http.debian.net/debian --debootstrapopts "--keyring=/usr/share/keyrings/debian-archive-keyring.gpg"
-dget http://http.debian.net/debian/pool/main/p/pystache/pystache_0.5.4-5.dsc
-sudo pbuilder --build --basetgz $HOME/base.tgz pystache_0.5.4-5.dsc
+sudo apt-get instll -f -y debian-archive-keyring/sid
+apt-get source ${1}/sid
+sudo pbuilder --build --basetgz $HOME/base.tgz ${1}.dsc
