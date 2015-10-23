9686edc82a79bc64022fede30d5ba46ab7137a10
Kouhei Maeda <mkouhei@palmtb.net>
2015-10-23 20:49:32 +0900

    Changes without cache.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- run-tests.sh
+++ run-tests.sh
@@ -5,18 +5,7 @@ sudo sh -c "echo deb http://archive.ubuntu.com/ubuntu wily universe >> /etc/apt/
 sudo apt-get update -qq
 sudo apt-get install -y pbuilder devscripts
 sudo apt-get install -y debian-archive-keyring/precise-backports
-if [ ! -f $HOME/.gnupg/debian-maintainers.gpg ]; then
-    (
-        install -d $HOME/.gnupg
-        install -d $HOME/tmp
-        cd $HOME/tmp
-        apt-get download debian-keyring/wily
-        ar -x debian-keyring_2015.08.13_all.deb
-        tar Jxf data.tar.xz
-        install usr/share/keyrings/debian-maintainers.gpg $HOME/.gnupg/
-    )
-fi
-test ! -f $HOME/base.tgz && sudo pbuilder --create --basetgz $HOME/base.tgz --distribution sid --components main --mirror http://http.debian.net/debian --debootstrapopts "--keyring=/usr/share/keyrings/debian-archive-keyring.gpg"
-sudo pbuilder --update --basetgz $HOME/base.tgz
+sudo apt-get install -y debian-keyring/wily
+sudo pbuilder --create --basetgz $HOME/base.tgz --distribution sid --components main --mirror http://http.debian.net/debian --debootstrapopts "--keyring=/usr/share/keyrings/debian-archive-keyring.gpg"
 dget http://http.debian.net/debian/pool/main/p/pystache/pystache_0.5.4-5.dsc
 sudo pbuilder --build pystache_0.5.4-5.dsc
