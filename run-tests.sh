a5d8df13f0df3d3df9c56834b8436def899657b4
Kouhei Maeda <mkouhei@palmtb.net>
2015-10-23 21:03:17 +0900

    Fixes specifying basetgz when building.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- run-tests.sh
+++ run-tests.sh
@@ -8,4 +8,4 @@ sudo apt-get install -y debian-archive-keyring/precise-backports
 sudo apt-get install -y debian-keyring/wily
 sudo pbuilder --create --basetgz $HOME/base.tgz --distribution sid --components main --mirror http://http.debian.net/debian --debootstrapopts "--keyring=/usr/share/keyrings/debian-archive-keyring.gpg"
 dget http://http.debian.net/debian/pool/main/p/pystache/pystache_0.5.4-5.dsc
-sudo pbuilder --build pystache_0.5.4-5.dsc
+sudo pbuilder --build --basetgz $HOME/base.tgz pystache_0.5.4-5.dsc
